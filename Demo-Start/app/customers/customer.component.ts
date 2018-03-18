cc273bb35b0ec2fad8f33da615c3310a27cc491a
Vishal Sakaria <vsakaria@gmail.com>
2018-03-18 15:29:21 +0000

    UI UPDATE module, component and template to use reactive extensions

--- Demo-Start/app/customers/customer.component.ts
+++ Demo-Start/app/customers/customer.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 import { Customer } from './customer';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
-    customer: Customer= new Customer();
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+export class CustomerComponent implements OnInit {
+    customerForm: FormGroup;
+    customer : Customer = new Customer();
+
+    ngOnInit () {
+        this.customerForm = new FormGroup({
+            firstName: new FormControl(),
+            lastName: new FormControl(),
+            email: new FormControl(),
+            sendCatalog: new FormControl(true)
+        });
+    }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
     }
  }
